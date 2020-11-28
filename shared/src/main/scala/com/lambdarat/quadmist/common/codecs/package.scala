57f6312edf983b93f22204f19223d0e56e4b8487
Roberto Serrano <r.semarcos@gmail.com>
2020-11-28 21:23:54 +0100

    Refactor and add JSON codecs

--- shared/src/main/scala/com/lambdarat/quadmist/common/codecs/package.scala
+++ shared/src/main/scala/com/lambdarat/quadmist/common/codecs/package.scala
@@ -16,75 +16,111 @@ import com.lambdarat.quadmist.common.game.GameEvent.{PlayerHand, PlayerMove}
 import com.lambdarat.quadmist.common.game._
 import com.lambdarat.quadmist.common.platform.UUID
 import com.lambdarat.quadmist.common.platform.UUID._
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.generic.semiauto.deriveCodec
 import io.circe.syntax._
-import io.circe.{Decoder, Encoder}
+import io.circe.{Codec, Decoder, Encoder}
 
 package object codecs {
-  /* ---- ENCODERS ---- */
-  implicit val powerEncoder       = Encoder.encodeInt.contramap[Power](_.toInt)
-  implicit val physicalDefEncoder = Encoder.encodeInt.contramap[PhysicalDef](_.toInt)
-  implicit val magicalDefEncoder  = Encoder.encodeInt.contramap[MagicalDef](_.toInt)
-  implicit val cardEncoder        = deriveEncoder[Card]
+  implicit val powerCodec       = Codec.from[Power](
+    Decoder.decodeInt.map(Power.apply),
+    Encoder.encodeInt.contramap[Power](_.toInt)
+  )
+  implicit val physicalDefCodec = Codec.from[PhysicalDef](
+    Decoder.decodeInt.map(PhysicalDef.apply),
+    Encoder.encodeInt.contramap[PhysicalDef](_.toInt)
+  )
+  implicit val magicalDefCodec  = Codec.from[MagicalDef](
+    Decoder.decodeInt.map(MagicalDef.apply),
+    Encoder.encodeInt.contramap[MagicalDef](_.toInt)
+  )
+  implicit val cardCodec        = deriveCodec[Card]
 
-  implicit val xaxisEncoder       = Encoder.encodeInt.contramap[XAxis](_.toInt)
-  implicit val yaxisEncoder       = Encoder.encodeInt.contramap[YAxis](_.toInt)
-  implicit val coordinatesEncoder = deriveEncoder[Coordinates]
+  implicit val xaxisCodec       = Codec.from[XAxis](
+    Decoder.decodeInt.map(XAxis.apply),
+    Encoder.encodeInt.contramap[XAxis](_.toInt)
+  )
+  implicit val yaxisCodec       = Codec.from[YAxis](
+    Decoder.decodeInt.map(YAxis.apply),
+    Encoder.encodeInt.contramap[YAxis](_.toInt)
+  )
+  implicit val coordinatesCodec = deriveCodec[Coordinates]
 
-  implicit val attackerEncoder       = deriveEncoder[Attacker]
-  implicit val defenderEncoder       = deriveEncoder[Defender]
-  implicit val attackerPointsencoder = Encoder.encodeInt.contramap[AttackerPoints](_.toInt)
-  implicit val defenderPointsencoder = Encoder.encodeInt.contramap[DefenderPoints](_.toInt)
-  implicit val attackerWinsEncoder   = Encoder.encodeBoolean.contramap[AttackerWins](_.toBool)
-  implicit val fightEncoder          = deriveEncoder[Fight]
+  implicit val attackerCodec       = deriveCodec[Attacker]
+  implicit val defenderCodec       = deriveCodec[Defender]
+  implicit val attackerPointsCodec = Codec.from[AttackerPoints](
+    Decoder.decodeInt.map(AttackerPoints.apply),
+    Encoder.encodeInt.contramap[AttackerPoints](_.toInt)
+  )
+  implicit val defenderPointsCodec = Codec.from[DefenderPoints](
+    Decoder.decodeInt.map(DefenderPoints.apply),
+    Encoder.encodeInt.contramap[DefenderPoints](_.toInt)
+  )
+  implicit val attackerWinsCodec   = Codec.from[AttackerWins](
+    Decoder.decodeBoolean.map(AttackerWins.apply),
+    Encoder.encodeBoolean.contramap[AttackerWins](_.toBool)
+  )
+  implicit val fightCodec          = deriveCodec[Fight]
 
-  implicit val boardSizeEncoder      = Encoder.encodeInt.contramap[BoardSize](_.toInt)
-  implicit val boardMaxBlocksEncoder = Encoder.encodeInt.contramap[BoardMaxBlocks](_.toInt)
-  implicit val cardMaxLevelEncoder   = Encoder.encodeInt.contramap[CardMaxLevel](_.toInt)
-  implicit val maxHandCardsEncoder   = Encoder.encodeInt.contramap[MaxHandCards](_.toInt)
-  implicit val settingsEncoder       = deriveEncoder[Settings]
+  implicit val boardSizeCodec      = Codec.from[BoardSize](
+    Decoder.decodeInt.map(BoardSize.apply),
+    Encoder.encodeInt.contramap[BoardSize](_.toInt)
+  )
+  implicit val boardMaxBlocksCodec = Codec.from[BoardMaxBlocks](
+    Decoder.decodeInt.map(BoardMaxBlocks.apply),
+    Encoder.encodeInt.contramap[BoardMaxBlocks](_.toInt)
+  )
+  implicit val cardMaxLevelCodec   = Codec.from[CardMaxLevel](
+    Decoder.decodeInt.map(CardMaxLevel.apply),
+    Encoder.encodeInt.contramap[CardMaxLevel](_.toInt)
+  )
+  implicit val maxHandCardsCodec   = Codec.from[MaxHandCards](
+    Decoder.decodeInt.map(MaxHandCards.apply),
+    Encoder.encodeInt.contramap[MaxHandCards](_.toInt)
+  )
+  implicit val settingsCodec       = deriveCodec[Settings]
 
-  implicit val cardClassIdEncoder = Encoder.encodeString.contramap[CardClass.Id](_.toUUID.show)
-  implicit val occupiedEncoder    = deriveEncoder[Occupied]
-  implicit val blockEncoder       = Encoder.encodeString.contramap[Block.type](_.entryName)
-  implicit val freeEncoder        = Encoder.encodeString.contramap[Free.type](_.entryName)
-  implicit val squareEncoder      = Encoder.instance[Square] {
-    case Free               => Free.asJson
-    case Block              => Block.asJson
-    case occupied: Occupied => occupied.asJson
-  }
-  implicit val boardEncoder       = deriveEncoder[Board]
-
-  implicit val gameTurnEncoder     = deriveEncoder[TurnFights]
-  implicit val turnStateEncoder    = deriveEncoder[TurnState]
-  implicit val gameErrorEncoder    = Encoder.encodeString.contramap[GameError](_.msg)
-  implicit val eventOutcomeEncoder = Encoder.encodeEither[GameError, TurnState]("error", "turn")
-
-  /* ---- DECODERS ---- */
-  implicit val cardClassIdDecoder   = Decoder[UUID].map(CardClass.Id.apply)
-  implicit val cardClassNameDecoder = Decoder.decodeString.map(CardClass.Name.apply)
-
-  implicit val playerIdDecoder = Decoder[UUID].map(Player.Id.apply)
-
-  implicit val cardIdDecoder            = Decoder[UUID].map(Card.Id.apply)
-  implicit val initialHandDecoder       = deriveDecoder[InitialHand]
-  implicit val playerRequestHandDecoder = deriveDecoder[PlayerHand]
-
-  implicit val xaxisDecoder       = Decoder.decodeInt.map(XAxis.apply)
-  implicit val yaxisDecoder       = Decoder.decodeInt.map(YAxis.apply)
-  implicit val coordinatesDecoder = deriveDecoder[Coordinates]
-
-  implicit val powerDecoder       = Decoder.decodeInt.map(Power.apply)
-  implicit val physicalDefDecoder = Decoder.decodeInt.map(PhysicalDef.apply)
-  implicit val magicalDefDecoder  = Decoder.decodeInt.map(MagicalDef.apply)
-  implicit val cardDecoder        = deriveDecoder[Card]
-  implicit val occupiedDecoder    = deriveDecoder[Occupied]
+  implicit val cardClassIdCodec = Codec.from[CardClass.Id](
+    Decoder[UUID].map(CardClass.Id.apply),
+    Encoder.encodeString.contramap[CardClass.Id](_.toUUID.show)
+  )
+  implicit val occupiedCodec    = deriveCodec[Occupied]
 
   implicit val squareDecoder: Decoder[Square] = Decoder.decodeString
     .emap(str => Square.withNameLowercaseOnlyEither(str).leftMap(_.notFoundName))
-    .or(occupiedDecoder.widen)
+    .or(Decoder[Occupied].widen)
+  implicit val squareEncoder                  = Encoder.instance[Square] {
+    case Free               => Free.entryName.asJson
+    case Block              => Block.entryName.asJson
+    case occupied: Occupied => occupied.asJson
+  }
+  implicit val squareCodec: Codec[Square]     = Codec.from[Square](squareDecoder, squareEncoder)
 
-  implicit val playerGameMoveDecoder = deriveDecoder[GameMovement]
-  implicit val playerMovementDecoder = deriveDecoder[PlayerMove]
-  implicit val gameEventDecoder      = deriveDecoder[GameEvent]
+  implicit val boardCodec = deriveCodec[Board]
+
+  implicit val cardClassNameCodec = Codec.from[CardClass.Name](
+    Decoder.decodeString.map(CardClass.Name.apply),
+    Encoder.encodeString.contramap[CardClass.Name](_.toStr)
+  )
+
+  implicit val playerIdCodec = Codec.from[Player.Id](
+    Decoder[UUID].map(Player.Id.apply),
+    Encoder.encodeString.contramap[Player.Id](_.toUUID.show)
+  )
+  implicit val cardIdCodec   = Codec.from[Card.Id](
+    Decoder[UUID].map(Card.Id.apply),
+    Encoder.encodeString.contramap[Card.Id](_.toUUID.show)
+  )
+
+  implicit val initialHandCodec       = deriveCodec[InitialHand]
+  implicit val playerRequestHandCodec = deriveCodec[PlayerHand]
+
+  implicit val playerGameMoveCodec = deriveCodec[GameMovement]
+  implicit val playerMovementCodec = deriveCodec[PlayerMove]
+  implicit val gameEventCodec      = deriveCodec[GameEvent]
+
+  implicit val gameTurnCodec  = deriveCodec[TurnFights]
+  implicit val turnStateCodec = deriveCodec[TurnState]
+
+  implicit val gameErrorCodec      = Encoder.encodeString.contramap[GameError](_.msg)
+  implicit val eventOutcomeEncoder = Encoder.encodeEither[GameError, TurnState]("error", "turn")
 }
